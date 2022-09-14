376bd02609053c9174cedb33b527ace4b3b0eb44
Maristela Bispo <maristelabispon@gmail.com>
2022-09-13 22:32:33 -0300

    validacao vencimento

--- js/script.js
+++ js/script.js
@@ -26,11 +26,27 @@ function colocarNumero(event) {
 dataInputVencimento.addEventListener('input', colocarVencimento);
 
 function colocarVencimento(event) {
-  if (event.target === dataInputMes) {
+  const regex = /\d{2}/g;
+  const ano = document.querySelector('.ano');
+
+  if (dataInputMes.value.length > 2) {
+    dataInputMes.value = dataInputMes.value.substr(1, 2);
+  }
+
+  if (event.target === dataInputMes && event.target.value.match(regex)) {
+    console.log('numero', event.target.value);
     document.querySelector('.mes').innerText = event.target.value;
   }
 
-  if (event.target === dataInputAno) {
-    document.querySelector('.ano').innerText = event.target.value;
+  if (dataInputAno.value.length > 2) {
+    dataInputAno.value = dataInputAno.value.substr(1, 2);
+  }
+
+  if (event.target === dataInputAno && event.target.value.match(regex)) {
+    ano.innerText = event.target.value;
+
+    // if (event.target.value.length > 3) {
+    //   ano.innerText = event.target.value.substr(1, 2);
+    // }
   }
 }
