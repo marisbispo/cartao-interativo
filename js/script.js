834c249ae7b3146ed1521acc2afcd9ead6d73212
Maristela Bispo <maristelabispon@gmail.com>
2022-09-20 19:39:27 -0300

    validacao feita

--- js/script.js
+++ js/script.js
@@ -15,30 +15,48 @@ const enviar = document.querySelector('[data-enviar]');
 nomeInput.addEventListener('input', colocarNome);
 
 function colocarNome(event) {
+  let erros = [];
   const regexNome = /^[A-Z a-z]+$/;
   if (event.target.value.match(regexNome)) {
     nomeCartao.innerText = event.target.value;
+    erros.splice(0, erros.length);
+    avisoErro('.erro-nome', erros);
+  } else {
+    erros.push('Digite o nome corretamente');
+    avisoErro('.erro-nome', erros);
   }
 }
 
 numeroInput.addEventListener('input', colocarNumero);
 
 function colocarNumero(event) {
+  let erros = [];
   let numeroFormatado = event.target.value
     .toString()
-    .replace(/\d{4}(?=.)/g, '$& '); //place a space after every 4 characters
+    .replace(/\d{4}(?=.)/g, '$& ');
   numeroCartao.textContent = numeroFormatado;
 
-  if (numeroFormatado.length === 16) {
-    return true;
-  } else {
-    return false;
+  if (event.target.value.length < 16) {
+    erros.push('Digite os numeros corretamente');
+    avisoErro('.erro-numero', erros);
+  }
+
+  if (event.target.value.length === 16) {
+    console.log('certo');
+    erros.splice(0, erros.length);
+    avisoErro('.erro-numero', erros);
+  }
+
+  if (event.target.value.length > 16) {
+    event.target.value = event.target.value.substr(0, 16);
+    numeroCartao.textContent = numeroFormatado.substr(0, 20);
   }
 }
 
 dataInputVencimento.addEventListener('input', colocarVencimento);
 
 function colocarVencimento(event) {
+  let erros = [];
   const regex = /\d{2}/g;
   const ano = document.querySelector('.ano');
 
@@ -59,27 +77,85 @@ function colocarVencimento(event) {
     ano.innerText = event.target.value;
   }
 
-  if ((dataInputMes.value.length === 2) & (dataInputAno.value.length === 2)) {
-    return true;
+  if (dataInputAno.value.length != 2 || dataInputMes.value.length != 2) {
+    erros.push('Digite a data de vencimento corretamente');
+    avisoErro('.erro-data', erros);
   } else {
-    return false;
+    erros.splice(0, erros.length);
+    avisoErro('.erro-data', erros);
   }
 }
 
 dataInputCVC.addEventListener('input', adicionarCVC);
 
 function adicionarCVC(event) {
+  let erros = [];
   numeroCVC.innerText = event.target.value;
   if (event.target.value.length > 3) {
     event.target.value = event.target.value.substr(0, 2);
     numeroCVC.innerText = event.target.value.substr(0, 20);
   }
-}
 
-function teste() {
-  return false;
+  if (event.target.value.length < 3) {
+    erros.push('Digite o cvc corretamente');
+    avisoErro('.erro-cvc', erros);
+  } else {
+    erros.splice(0, erros.length);
+    avisoErro('.erro-cvc', erros);
+  }
 }
 
 enviar.addEventListener('click', enviarDados);
 
-function enviarDados(event) {}
+function enviarDados(event) {
+  event.preventDefault();
+  avisoErro('.erro-Msg', validaCampos());
+  if (avisoErro('.erro-Msg', validaCampos())) {
+    window.location.href = 'enviado.html';
+  }
+}
+
+function avisoErro(classe, erros) {
+  const erroMsg = document.querySelector(classe);
+
+  if (erros.length >= 1) {
+    erroMsg.innerHTML = '';
+    erros.forEach((erro) => {
+      const li = document.createElement('li');
+      li.innerText = erro;
+      erroMsg.appendChild(li);
+    });
+  } else if (erros.length < 1) {
+    console.log(erroMsg.innerText);
+    erroMsg.innerText = '';
+    return true;
+  }
+}
+
+function validaCampos() {
+  let erros = [];
+  if (dataInputMes.value.length != 2) {
+    erros.push('Digite o Campo do mes corretamente!');
+  }
+  if (dataInputAno.value.length != 2) {
+    erros.push('Digite o Campo do ano corretamente!');
+  }
+
+  if (dataInputCVC.value.length != 3) {
+    erros.push('Digite o codigo de segurança corretamente!');
+  }
+
+  if (dataInputCVC.value.length != 3) {
+    erros.push('Digite o codigo de segurança corretamente!');
+  }
+
+  if (numeroInput.value.length != 16) {
+    erros.push('Digite o numero do cartão corretamente!');
+  }
+
+  if (nomeInput.value.length < 5) {
+    erros.push('Digite o nome corretamente!');
+  }
+
+  return erros;
+}
